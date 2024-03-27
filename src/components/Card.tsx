172cb75c899736aaa06a8a685911d529a9a19244
Alex <31933549+crypt096@users.noreply.github.com>
2024-03-27 11:15:27 +0100

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,8 @@
 const Card = ({ children }) => {
   return (
-    <div>Card</div>
+    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+      {children}
+    </div>
   )
 }
 export default Card
