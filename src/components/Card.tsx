1eb4f436cbf8f9df1a07993e62f4fbdc8a866aff
Alex <31933549+crypt096@users.noreply.github.com>
2024-03-27 11:19:36 +0100

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,9 @@
-const Card = ({ children, bg = 'bg-gray-100' }) => {
+interface CardProps {
+  children: React.ReactNode,
+  bg: string;
+}
+
+const Card = ({ children, bg = 'bg-gray-100' }: CardProps) => {
   return (
     <div className={`${bg} p-6 rounded-lg shadow-md`}>
       {children}
