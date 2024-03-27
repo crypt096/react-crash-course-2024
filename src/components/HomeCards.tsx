99ee648fbe3ed357b848480975f4d8f24d864654
Alex <31933549+crypt096@users.noreply.github.com>
2024-03-27 11:16:38 +0100

    Update HomeCards.tsx

--- src/components/HomeCards.tsx
+++ src/components/HomeCards.tsx
@@ -1,10 +1,12 @@
+import Card from "./Card";
+
 const HomeCards = () => {
   return (
     <>
       <section className="py-4">
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <Card>
               <h2 className="text-2xl font-bold">For Developers</h2>
               <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
@@ -15,7 +17,7 @@ const HomeCards = () => {
               >
                 Browse Jobs
               </a>
-            </div>
+            </Card>
             <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
               <h2 className="text-2xl font-bold">For Employers</h2>
               <p className="mt-2 mb-4">
