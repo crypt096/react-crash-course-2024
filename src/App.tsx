9a9b998fef17ecf9233a98ee5983b7e1219f3b4f
Alex <31933549+crypt096@users.noreply.github.com>
2024-03-29 10:58:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 );
